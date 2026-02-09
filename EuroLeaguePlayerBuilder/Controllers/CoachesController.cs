2edbbd977e6d25f1e316b331405c9a78f10788e3
chirp <chirp@NEKVO_ZMII4EE>
2026-02-09 23:35:26 +0200

    Added the Coaches Controller with a view that displays all coaches

---
+++ EuroLeaguePlayerBuilder/Controllers/CoachesController.cs
@@ -0,0 +1,35 @@
+﻿using EuroLeaguePlayerBuilder.Data;
+using EuroLeaguePlayerBuilder.ViewModels.Coaches;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EuroLeaguePlayerBuilder.Controllers
+{
+    public class CoachesController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public CoachesController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<AllCoachesViewModel> coaches = _dbContext
+                .Teams
+                .Select(t => new AllCoachesViewModel
+                {
+                    FirstName = t.Coach.FirstName,
+                    LastName = t.Coach.LastName,
+                    TitlesWon = t.Coach.TitlesWon,
+                    TeamId = t.Id,
+                    TeamLogoPath = t.LogoPath
+                })
+                .AsNoTracking()
+                .ToList();
+
+            return View(coaches);
+        }
+    }
+}
---
+++ EuroLeaguePlayerBuilder/ViewModels/Coaches/AllCoachesViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace EuroLeaguePlayerBuilder.ViewModels.Coaches
+{
+    public class AllCoachesViewModel
+    {
+        public string FirstName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+
+        public int TitlesWon { get; set; }
+
+        public int TeamId { get; set; }
+
+        public string TeamLogoPath { get; set; } = null!;
+    }
+}
